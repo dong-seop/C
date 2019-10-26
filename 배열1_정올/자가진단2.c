5481d1696f615881cecc153364a9395ebdc2b043
dong-seop <k_ds10282naver.com>
2019-10-26 14:47:01 +0900

    정올 배열 문제풀이

---
+++ 배열1_정올/자가진단2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//정수 10 개를 저장할 수 있는 배열을 만들어서 
+//1 부터 10 까지를 대입하고 차례대로 출력하는 프로그램을 작성하시오.
+
+int main()
+{
+	int arr[10];
+	for (int i = 0; i < 10; i++)
+		arr[i] = i + 1;
+	for (int i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
