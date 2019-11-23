89c92bd1e5f10cb2ab232b290e7906365dd4794b
dong-seop <k_ds10282naver.com>
2019-11-23 11:51:27 +0900

    insertion sort

---
+++ sort/insertionsort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void swap(int* a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void PrintArr(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+int* CreateArr(int size, int range)
+{
+	int* arr = (int*)malloc(sizeof(int)*size);
+	for (int i = 0; i < size; i++)
+		arr[i] = rand() % range + 1;
+	return arr;
+}
+int main()
+{
+	int size = 20;
+	srand(time(NULL));
+	int* arr = CreateArr(size, 200);
+	PrintArr(arr, size);
+
+	// select sort
+	int min = 0;
+
+	for (int i = 1; i < size; i++) 
+	{
+		int key=arr[i], j;
+		for (j = i - 1; j >= 0 && arr[j] > key; j--) 
+			arr[j + 1] = arr[j];
+		arr[j + 1] = key;
+	}
+	
+	PrintArr(arr, size);
+	return 0;
+}
